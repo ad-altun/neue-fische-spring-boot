ae1e2ee9a2e4b7797071c767a60931fd6f61c6ce
ad-altun <adaltun@web.de>
2025-07-17 17:37:23 +0200

    Initial commit

---
+++ src/main/java/org/example/NeueFischeSpringBootApplication.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeueFischeSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeueFischeSpringBootApplication.class, args);
+	}
+
+}
