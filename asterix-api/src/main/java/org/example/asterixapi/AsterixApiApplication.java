d58e9ebf267909802f5983252386d5594995c6d1
ad-altun <adaltun@web.de>
2025-07-21 10:26:56 +0200

    asterix-api: - initial commit

---
+++ asterix-api/src/main/java/org/example/asterixapi/AsterixApiApplication.java
@@ -0,0 +1,12 @@
+package org.example.asterixapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsterixApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AsterixApiApplication.class, args);
+    }
+}
