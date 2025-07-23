8b86079d1eadf10b30c5b103a27c1608b20a5b1c
ad-altun <adaltun@web.de>
2025-07-23 11:48:42 +0200

    spring-recap-todo: - add 'deleteToDoById' method

--- spring-recap-todo/src/main/java/org/example/springrecaptodo/repository/ToDoRepository.java
+++ spring-recap-todo/src/main/java/org/example/springrecaptodo/repository/ToDoRepository.java
@@ -4,6 +4,8 @@ import org.example.springrecaptodo.model.ToDo;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+
 @Repository
 public interface ToDoRepository extends MongoRepository<ToDo, String> {
+    void deleteToDoById(String id);
 }
