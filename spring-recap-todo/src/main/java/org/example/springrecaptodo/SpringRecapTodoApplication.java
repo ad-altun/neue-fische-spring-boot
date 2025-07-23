12cbf409e027316c32576040ef89370de3c7d3a9
ad-altun <adaltun@web.de>
2025-07-23 07:29:35 +0200

    spring-recap-todo: - initial commit

---
+++ spring-recap-todo/src/main/java/org/example/springrecaptodo/SpringRecapTodoApplication.java
@@ -0,0 +1,13 @@
+package org.example.springrecaptodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRecapTodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRecapTodoApplication.class, args);
+	}
+
+}
---
+++ spring-recap-todo/src/test/java/org/example/springrecaptodo/SpringRecapTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springrecaptodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRecapTodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
