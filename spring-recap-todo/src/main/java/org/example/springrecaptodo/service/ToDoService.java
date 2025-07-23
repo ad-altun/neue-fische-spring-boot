f090085863f5312042c6ff4270472205e53d53d5
ad-altun <adaltun@web.de>
2025-07-23 09:56:02 +0200

    spring-recap-todo: - add todo service

---
+++ spring-recap-todo/src/main/java/org/example/springrecaptodo/service/ToDoService.java
@@ -0,0 +1,12 @@
+package org.example.springrecaptodo.service;
+
+import org.example.springrecaptodo.model.ToDo;
+import org.example.springrecaptodo.repository.ToDoRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ToDoService {
+
+}
