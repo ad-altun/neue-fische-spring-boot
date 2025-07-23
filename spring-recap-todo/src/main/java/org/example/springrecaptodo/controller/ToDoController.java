9dd502e2963b7d03ed0ff3bde1890c411ff5a0e8
ad-altun <adaltun@web.de>
2025-07-23 10:19:09 +0200

    spring-recap-todo: - use findAll method

--- spring-recap-todo/src/main/java/org/example/springrecaptodo/controller/ToDoController.java
+++ spring-recap-todo/src/main/java/org/example/springrecaptodo/controller/ToDoController.java
@@ -1,5 +1,8 @@
 package org.example.springrecaptodo.controller;
 
+import org.example.springrecaptodo.model.ToDo;
+import org.example.springrecaptodo.service.ToDoService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +11,15 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/todo")
 public class ToDoController {
+
+    ToDoService toDoService;
+
+    public  ToDoController(ToDoService service) {
+        this.toDoService = service;
+    }
+
+    @GetMapping
+    public List<ToDo> getToDos() {
+        return toDoService.getToDos();
+    }
 }
