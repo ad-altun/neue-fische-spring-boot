fb9558e2942cc56709a4692e9f3b81cf8aad6936
ad-altun <adaltun@web.de>
2025-07-24 10:28:40 +0200

    restclient-rickandmorty-api: initial commit

---
+++ restclient-rickandmorty-api/src/main/java/org/example/restclientrickandmortyapi/RestclientRickandmortyApiApplication.java
@@ -0,0 +1,13 @@
+package org.example.restclientrickandmortyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestclientRickandmortyApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestclientRickandmortyApiApplication.class, args);
+    }
+
+}
---
+++ restclient-rickandmorty-api/src/test/java/org/example/restclientrickandmortyapi/RestclientRickandmortyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.restclientrickandmortyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestclientRickandmortyApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
