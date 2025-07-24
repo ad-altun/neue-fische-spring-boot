9941d04f032a1dde14a6d2004f377614fa0e0060
ad-altun <adaltun@web.de>
2025-07-24 11:29:18 +0200

    restclient-rickandmorty-api: add models

---
+++ restclient-rickandmorty-api/src/main/java/org/example/restclientrickandmortyapi/model/CharacterModel.java
@@ -0,0 +1,4 @@
+package org.example.restclientrickandmortyapi.model;
+
+public record CharacterModel(Integer id, String name, String species) {
+}
---
+++ restclient-rickandmorty-api/src/main/java/org/example/restclientrickandmortyapi/model/CharacterResults.java
@@ -0,0 +1,6 @@
+package org.example.restclientrickandmortyapi.model;
+
+import java.util.List;
+
+public record CharacterResults(List<CharacterModel> results) {
+}
