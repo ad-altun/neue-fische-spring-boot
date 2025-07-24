7e0c787c587c3346ee31abcf67ac8244babebe93
ad-altun <adaltun@web.de>
2025-07-24 11:29:59 +0200

    restclient-rickandmorty-api: add controller with the get all characters functionality

---
+++ restclient-rickandmorty-api/src/main/java/org/example/restclientrickandmortyapi/controller/CharacterController.java
@@ -0,0 +1,25 @@
+package org.example.restclientrickandmortyapi.controller;
+
+import org.example.restclientrickandmortyapi.model.CharacterModel;
+import org.example.restclientrickandmortyapi.service.CharacterService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/")
+public class CharacterController {
+
+    private final CharacterService characterService;
+
+    public CharacterController(CharacterService characterService) {
+        this.characterService = characterService;
+    }
+
+    @GetMapping("/character")
+    public List<CharacterModel> getCharacters () {
+        return characterService.getAllCharacters();
+    }
+}
