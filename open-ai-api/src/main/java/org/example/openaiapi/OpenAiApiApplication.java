6f22fd05707ac26f72b1411369001ecb36ebaec4
ad-altun <adaltun@web.de>
2025-07-25 13:46:35 +0200

    open-ai-api: initial commit

---
+++ open-ai-api/src/main/java/org/example/openaiapi/OpenAiApiApplication.java
@@ -0,0 +1,13 @@
+package org.example.openaiapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenAiApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenAiApiApplication.class, args);
+    }
+
+}
---
+++ open-ai-api/src/test/java/org/example/openaiapi/OpenAiApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.openaiapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenAiApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
